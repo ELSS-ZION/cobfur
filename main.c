d482fd5f65b48fcd78b6c7a0acb43c297524fa7c
ElssZion <elsszion@gmail.com>
2024-01-10 17:59:13 +0800

    main.c: s = "aaaa"      "bbbb"; s1 = formatString("111%s222", "yy");

--- main.c
+++ main.c
@@ -4,7 +4,9 @@ int main()
     char arr[] = "xxxx";
     const char *p = NULL;
 
-    "aaaa"      "bbbb";
+    const char *s = "aaaa"      "bbbb";
+    const char *s1 = formatString("111%s222", "yy");
+
     p = "cccc";
     
     for ( i = 1; i <= 9; i++ ) {
